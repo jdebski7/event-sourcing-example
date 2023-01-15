9e46603f826f48ba8bf83bf2f1d511f5f4cc9081
Jakub Dębski <jdebski@airnauts.com>
2023-01-15 17:19:54 +0100

    Setup telemetry stack (#3)
    * Setup telemetry stack
    
    * Setup cicd

--- src/Gateway/Gateway.Api/Program.cs
+++ src/Gateway/Gateway.Api/Program.cs
@@ -1,4 +1,10 @@
+using System.Reflection;
 using MassTransit;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,17 +14,58 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddMassTransit(config =>
 {
-    config.UsingRabbitMq();
+    config.UsingRabbitMq((_, mqConfig) =>
+    {
+        mqConfig.Host("rabbitmq", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(b =>
+    {
+        b.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+                .AddTelemetrySdk())
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddOtlpExporter(otlpExporterOptions =>
+            {
+                builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+            });
+    })
+    .WithTracing(b =>
+    {
+        b.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+                .AddTelemetrySdk())
+            .AddSource("MassTransit")
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(otlpExporterOptions =>
+            {
+                builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+            });
+    }).StartWithHost();
+
+builder.Logging.AddOpenTelemetry(c =>
+{
+    c.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+            .AddTelemetrySdk())
+        .AddOtlpExporter(otlpExporterOptions =>
+        {
+            builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+        });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
--- src/Ordering/Ordering.Api/Program.cs
+++ src/Ordering/Ordering.Api/Program.cs
@@ -1,3 +1,9 @@
+using System.Reflection;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Ordering.Infrastructure.DependencyInjections;
 using Ordering.Infrastructure.DependencyInjections.Settings;
 
@@ -14,6 +20,44 @@ builder.Services.AddInfrastructure(b =>
     b.AddEventBus();
     b.AddMongoEventDatabase(mongoEventDatabase);
     b.AddMongoReadDatabase(mongoReadDatabase);
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(b =>
+    {
+        b.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+                .AddTelemetrySdk())
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddOtlpExporter(otlpExporterOptions =>
+            {
+                builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+            });
+    })
+    .WithTracing(b =>
+    {
+        b.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+                .AddTelemetrySdk())
+            .AddSource("MassTransit")
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(otlpExporterOptions =>
+            {
+                builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+            });
+    }).StartWithHost();
+
+builder.Logging.AddOpenTelemetry(c =>
+{
+    c.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService(Assembly.GetExecutingAssembly().GetName().Name!)
+            .AddTelemetrySdk())
+        .AddOtlpExporter(otlpExporterOptions =>
+        {
+            builder.Configuration.GetSection("OpenTelemetry:OtlpExporter").Bind(otlpExporterOptions);
+        });
 });
 
 var app = builder.Build();
